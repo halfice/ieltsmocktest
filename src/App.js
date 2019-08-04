601fe140deb63a7dd7818d07ca4878bf2bc3bc86
halfice <axix.szabist@gmail.com>
2019-08-04 16:58:56 +0400

    adding expereicce components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,136 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Writing } from './Writing';
+import listenning from './listenning.png'
+import reading from './reading.png'
+import writing from './writing.png'
+import writing2 from './writing2.png'
+import home from './home.png'
+export class App extends React.Component {
+  //function App() {
+  constructor(props) {
+    super(props);
+    this.state = {
+      TimeTogo: this.props.TimeTogo,
+      WordCount: 0,
+      WritingTask1: false,
+      WritingTask2: false,
+      listenning: false,
+      reading: false,
+      Home:true,
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+    };
+  }
+
+  changelitens() {
+    this.setState({
+      listenning: true,
+      reading: false,
+      WritingTask1:false,
+      WritingTask2:false,
+      Home:false,
+
+    });
+  }
+
+  changereading() {
+    this.setState({
+      reading: true,
+      listenning: false,
+      WritingTask1:false,
+      WritingTask2:false,
+      Home:false,
+    });
+  }
+
+  changewriting() {
+    this.setState({
+      WritingTask1: true,
+      listenning: false,
+      reading: false,
+      WritingTask2:false,
+      Home:false,
+    });
+  }
+
+  changewriting2() {
+    this.setState({
+      WritingTask2: true,
+      listenning: false,
+      reading: false,
+      WritingTask1:false,
+      Home:false,
+
+    });
+  }
+
+  gohome() {
+    this.setState({
+      WritingTask2: false,
+      listenning: false,
+      reading: false,
+      WritingTask1:false,
+      Home:true,
+
+    });
+  }
+  
+
+
+  render() {
+    return (
+      <div className="App">
+        <div className="App">
+          {
+            this.state.Home==false &&
+            
+            <img src={home} alt="Logo" className="Imageswidth" onClick={this.gohome.bind(this)} />
+          }
+
+          <div>
+            <h3>IELTS MOCK TEST - COMPUTER DELIVERED</h3>
+            <div>
+              <h4>This is to practice and my little efforts for computer delivered IELTS test. This is only to help candidate to know how the test will be</h4>
+              <h5>I am keep updating with the newest test in all sections.</h5>
+            </div>
+
+            <br></br>
+            {
+              this.state.Home==true &&
+            <div >
+              <div className="Imagesdiv"> 
+              <img src={listenning} alt="Logo" className="Imageswidth" onClick={this.changelitens.bind(this)} /></div>
+              <div className="Imagesdiv">  <img src={reading} alt="Logo" className="Imageswidth" onClick={this.changereading.bind(this)} /></div>
+              <div className="Imagesdiv">  <img src={writing} alt="Logo" className="Imageswidth" onClick={this.changewriting.bind(this)} /></div>
+              <div className="Imagesdiv">  <img src={writing2} alt="Logo" className="Imageswidth" onClick={this.changewriting2.bind(this)} /></div>
+            </div>
+            }
+          </div>
+
+
+          {this.state.WritingTask1 == true &&
+            <Writing TimeTogo="1200000"  placeholder="Writing Task 1 - 20 Minutes" />
+          }
+
+          {this.state.WritingTask2 == true &&
+            <Writing TimeTogo="2400000"   placeholder="Writing Task 2 - 40 Minutes"  />
+          }
+
+          {this.state.listenning == true &&
+            <h3>Listen</h3>
+          }
+
+          {this.state.reading == true &&
+            <h3>Reading</h3>
+          }
+        </div>
+      </div>
+    )
+  };
+
 }
 
 export default App;
---
+++ src/Writing.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import 'react-dom';
+import './App.css';
+import Countdown from 'react-countdown-now';
+import { Form, TextArea } from 'semantic-ui-react'
+export class Writing extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            TimeTogo: this.props.TimeTogo,
+            WordCount: 0,
+            placeholders:this.props.placeholder,
+
+        };
+    }
+
+    handleChange() {
+        var tmp=this.state.WordCount+1;
+        this.setState({
+            WordCount: tmp,
+        });
+    }
+    render() {
+        console.log(this.props.TimeTogo);
+        const Completionist = () => <span>You are good to go!</span>;
+        return (
+            <div><h2>Writing Task 1 - General Test IELTS</h2>
+                <h3>Time Left:<Countdown date={Date.now() + parseInt(this.state.TimeTogo)} >
+                    <Completionist />
+
+                </Countdown></h3>
+                <br></br>
+                <TextArea onChange={this.handleChange.bind(this)} className="paddingtextbox" rows={10} placeholder={this.state.placeholders} />
+                <div>
+
+                </div>
+                <h3>Total Word : {this.state.WordCount}</h3>
+            </div>
+        );
+    }
+};
+
+
